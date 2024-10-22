e34b72f2905832494965f98f592c2e64d4840c13
AlanMartinezDev <alanmartiola@gmail.com>
2024-10-22 16:29:51 +0200

    Finalizando CSS del dashboard

--- views/templates/sidebar.php
+++ views/templates/sidebar.php
@@ -2,8 +2,8 @@
     <h2>TaskFlow</h2>
 
     <nav class="sidebar-nav">
-        <a href="/dashboard">Proyectos</a>
-        <a href="/crear-proyecto">Crear proyecto</a>
-        <a href="/perfil">Perfil</a>
+        <a href="/dashboard" class="<?php echo ($titulo === 'Proyectos') ? 'activo' : ''; ?>">Proyectos</a>
+        <a href="/crear-proyecto" class="<?php echo ($titulo === 'Crear proyecto') ? 'activo' : ''; ?>">Crear proyecto</a>
+        <a href="/perfil" class="<?php echo ($titulo === 'Perfil') ? 'activo' : ''; ?>">Perfil</a>
     </nav>
 </aside>
