42dac1696b0ddbc8704ca4f0d8f26d66c5319de0
AlanMartinezDev <alanmartiola@gmail.com>
2024-10-25 15:59:14 +0200

    Añadir tabla de proyectos

--- taskflow_db.sql
+++ taskflow_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 14-10-2024 a las 15:37:14
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Host: localhost:3306
+-- Generation Time: Oct 25, 2024 at 01:58 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,43 +18,79 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `taskflow_db`
+-- Database: `taskflow_db`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Table structure for table `proyectos`
+--
+
+CREATE TABLE `proyectos` (
+  `id` int NOT NULL,
+  `proyecto` varchar(60) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `url` varchar(32) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `propietarioId` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
 --
 
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(30) DEFAULT NULL,
-  `email` varchar(60) DEFAULT NULL,
-  `password` varchar(60) DEFAULT NULL,
-  `token` varchar(15) DEFAULT NULL,
+  `id` int NOT NULL,
+  `nombre` varchar(30) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email` varchar(60) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `password` varchar(60) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `token` varchar(15) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `confirmado` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `usuarios`
+-- Indexes for table `proyectos`
+--
+ALTER TABLE `proyectos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `propietarioId` (`propietarioId`);
+
+--
+-- Indexes for table `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `usuarios`
+-- AUTO_INCREMENT for table `proyectos`
+--
+ALTER TABLE `proyectos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `proyectos`
+--
+ALTER TABLE `proyectos`
+  ADD CONSTRAINT `proyectos_ibfk_1` FOREIGN KEY (`propietarioId`) REFERENCES `usuarios` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
